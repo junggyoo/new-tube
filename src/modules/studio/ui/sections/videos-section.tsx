69d8ea3ee514d8a5c7ba85ee74169fe44bdd5398
BaeJungKyu <junggyoo.1@gmail.com>
2025-06-08 10:47:31 +0900

    feat: 무한 스크롤 로딩 추가

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -1,17 +1,85 @@
 "use client";
 
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
 import { trpc } from "@/trpc/client";
 import { DEFAULT_LIMIT } from "@/constants";
+import { InfiniteScroll } from "@/components/infinite-scroll";
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+import Link from "next/link";
 
 export function VideosSection() {
-	const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery(
-		{
-			limit: DEFAULT_LIMIT,
-		},
-		{
-			getNextPageParam: (lastPage) => lastPage.nextCursor,
-		}
+	return (
+		<Suspense fallback={<div>Loading...</div>}>
+			<ErrorBoundary fallback={<div>Error</div>}>
+				<VideosSectionSuspense />
+			</ErrorBoundary>
+		</Suspense>
 	);
+}
 
-	return <div>{JSON.stringify(data)}</div>;
+function VideosSectionSuspense() {
+	const [videos, { hasNextPage, isFetchingNextPage, fetchNextPage }] =
+		trpc.studio.getMany.useSuspenseInfiniteQuery(
+			{
+				limit: DEFAULT_LIMIT,
+			},
+			{
+				getNextPageParam: (lastPage) => lastPage.nextCursor,
+			}
+		);
+
+	return (
+		<div>
+			<div className="border-y">
+				<Table>
+					<TableHeader>
+						<TableRow>
+							<TableHead className="pl-6 w-[510px]">Video</TableHead>
+							<TableHead>Visibility</TableHead>
+							<TableHead>Status</TableHead>
+							<TableHead className="text-right">Views</TableHead>
+							<TableHead className="text-right">Comments</TableHead>
+							<TableHead className="text-right">Likes</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						{videos.pages.flatMap((page) =>
+							page.items.map((video) => (
+								<Link
+									key={video.id}
+									href={`/studio/videos/${video.id}`}
+									legacyBehavior
+								>
+									<TableRow className="cursor-pointer">
+										<TableCell>{video.title}</TableCell>
+										<TableCell>visibility</TableCell>
+										<TableCell>status</TableCell>
+										<TableCell>date</TableCell>
+										<TableCell>views</TableCell>
+										<TableCell>comments</TableCell>
+										<TableCell>likes</TableCell>
+									</TableRow>
+								</Link>
+							))
+						)}
+					</TableBody>
+				</Table>
+				<InfiniteScroll
+					isManual
+					hasNextPage={hasNextPage}
+					isFetchingNextPage={isFetchingNextPage}
+					fetchNextPage={fetchNextPage}
+				/>
+			</div>
+		</div>
+	);
 }
